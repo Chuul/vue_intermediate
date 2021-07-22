5451be6ee9337a9e956fa701608e25d95cd3e064
Chuul <bethejs30@gmail.com>
2021-07-22 14:23:03 +0900

    until modulize_step1

--- vue-todo/src/components/store/store.js
+++ vue-todo/src/components/store/store.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as mutations from './mutations'
 
 Vue.use(Vuex)
 
@@ -21,29 +23,6 @@ export const store = new Vuex.Store({
     state : {
         todoItems : storage.fetch()
     },
-    getters : {
-        storedTodoItems(state) {
-            return state.todoItems;
-        }
-    },
-    mutations : {
-        addOneItem : function(state, todoItem){
-            let obj = {completed : false, item : todoItem}
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj)
-          },
-          removeOneItem : function(state, payload){
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index, 1);
-          },
-          checkOneItem : function(state, payload){
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            localStorage.removeItem(payload.todoItem.item)
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-          },
-          clearAllItem : function(state){
-            localStorage.clear();
-            state.todoItems = [];
-          }
-    }
+    getters,
+    mutations
 })
